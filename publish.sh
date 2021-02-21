b1491336ce7a075dff3792cc4b988c21ca525172
LuisEnMarroquin <mluis651@gmail.com>
2021-02-21 11:41:16 -0600

    More keywords in package.json
    * Improved publish script
    * Updated version also in package-lock.json

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo Publishing to NPM and creating tag
+printf "\nPublishing to NPM and creating tag \n\n"
 
 echo Remember to-do before:
 echo - Change package version number
 echo - Git add and commit your changes
 
 # Waiting for X seconds
-sleep 15
+sleep 10
 
 # TypeScript compile and uglify code
 npm run compile
@@ -16,7 +16,7 @@ npm run compile
 npm publish
 
 # Tag the new release
-git tag -a -m "Published v1.1.9" v1.1.9
+git tag -a -m "Published v1.2.0" v1.2.0
 
 # Push commit and tags
 git push --follow-tags

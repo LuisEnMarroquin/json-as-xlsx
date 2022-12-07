ea56bb2bda3dea6cbba3622c04d17348ffe0ebb8
LuisEnMarroquin <mluis651@gmail.com>
2022-12-06 22:46:04 -0600

    vite.config optimizeDeps works for dev mode but not when build
    https://github.com/vitejs/vite/issues/8914

--- packages/example-reactjs/src/App.tsx
+++ packages/example-reactjs/src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import "./App.css"
+
+import Example from "./Example"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,16 +19,13 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Example />
     </div>
   )
 }
--- packages/example-reactjs/vite.config.ts
+++ packages/example-reactjs/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 5600,
+  },
+  optimizeDeps: {
+    include: ["json-as-xlsx"],
+  },
 })

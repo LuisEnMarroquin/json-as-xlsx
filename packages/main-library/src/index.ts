530922df2dda0711b7c81c5235a3ec2b1121e907
mbaranturkmen <kilexey@gmail.com>
2023-08-19 08:50:35 +0100

    fix

--- packages/main-library/src/index.ts
+++ packages/main-library/src/index.ts
@@ -36,8 +36,8 @@ export type IWorkbookCallback = (workbook: WorkBook) => void
 
 export { utils, WorkBook, WorkSheet }
 
-export const getContentProperty = (content: IContent | null, property: string): string | number | boolean | Date | IContent => {
-  const accessContentProperties = (content: IContent | null, properties: string[]): string | number | boolean | Date | IContent => {
+export const getContentProperty = (content: IContent | null, property: string): string | number | boolean | Date | IContent | null => {
+  const accessContentProperties = (content: IContent | null, properties: string[]): string | number | boolean | Date | IContent | null => {
     if (!content) {
       return ""
     }
